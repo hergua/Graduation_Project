06366544eea3f9c5b21a9d17711983cfa7f72c8b
Hergua <hergua@qq.com>
2019-03-21 13:58:55 +0800

    增加了视频服务和图片服务的模块

---
+++ service-module-picture/src/test/java/cn/hergua/servicemodule/ServiceModuleApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.hergua.servicemodule;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceModuleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
